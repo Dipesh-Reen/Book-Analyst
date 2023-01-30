fbef49281620729cf89b940f45322d62ce310905
Dipesh-Reen <dipeshreen@gmail.com>
2023-01-30 02:16:20 +0000

    Added get_volume and get_volume_info functionality

--- google_api_connection.py
+++ google_api_connection.py
@@ -1,5 +1,18 @@
-def get_volumes():
-    pass
+''' A list of methods which integrate with Google APIs'''
 
-def get_volume_info():
-    pass
+import json
+import requests
+
+BASE_URI = "https://www.googleapis.com/books/v1/volumes"
+
+def get_volumes(query: str):
+    ''' Get list of items that match the requested query'''
+    response = requests.get(f"{BASE_URI}?q={query}", timeout=1000)
+    return json.loads(response.text)
+
+
+def get_volume_info(volume_id: str):
+    ''' Get specific information about the provided volume_id'''
+    response = requests.get(f"{BASE_URI}/{volume_id}", timeout=1000)
+    return json.loads(response.text)
+    
