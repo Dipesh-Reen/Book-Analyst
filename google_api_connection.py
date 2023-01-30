e4df336343b21f353e65c1b1ded8666a19cf4e43
Dipesh Reen <dipeshreen@gmail.com>
2023-01-29 19:29:44 -0600

    Removed json as an import

--- google_api_connection.py
+++ google_api_connection.py
@@ -1,6 +1,5 @@
 """ A list of methods which integrate with Google APIs"""
 
-import json
 import requests
 
 BASE_URI = "https://www.googleapis.com/books/v1/volumes"
@@ -9,10 +8,13 @@ BASE_URI = "https://www.googleapis.com/books/v1/volumes"
 def get_volumes(query: str):
     """ Get list of items that match the requested query"""
     response = requests.get(f"{BASE_URI}?q={query}", timeout=1000)
-    return json.loads(response.text)
+    return response.json()
 
 
 def get_volume_info(volume_id: str):
     """ Get specific information about the provided volume_id"""
     response = requests.get(f"{BASE_URI}/{volume_id}", timeout=1000)
-    return json.loads(response.text)
+    return response.json()
+
+print(get_volume_info("t_ZYYXZq4RgC"))
+    
